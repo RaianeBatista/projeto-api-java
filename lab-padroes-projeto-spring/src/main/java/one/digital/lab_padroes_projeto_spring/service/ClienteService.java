bd5d61cf91d66810bb1035ad81be0faab7e5c3ae
RaianeBatista <202311169804@alunos.estacio.br>
2025-12-26 19:02:25 -0300

    feat: external api integration with feign and service layer

---
+++ lab-padroes-projeto-spring/src/main/java/one/digital/lab_padroes_projeto_spring/service/ClienteService.java
@@ -0,0 +1,23 @@
+package one.digital.lab_padroes_projeto_spring.service;
+
+
+import one.digital.lab_padroes_projeto_spring.model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter múltiplas implementações dessa mesma
+ * interface.
+ * * @author RaianeBatista
+ */
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
