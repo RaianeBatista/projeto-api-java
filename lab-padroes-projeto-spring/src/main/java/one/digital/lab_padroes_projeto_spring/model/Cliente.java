f2c4aed24ad4f408e7f67054067aab07efacd10b
RaianeBatista <202311169804@alunos.estacio.br>
2025-12-26 19:02:00 -0300

    feat: implement domain model and data access layer

---
+++ lab-padroes-projeto-spring/src/main/java/one/digital/lab_padroes_projeto_spring/model/Cliente.java
@@ -0,0 +1,31 @@
+package one.digital.lab_padroes_projeto_spring.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return  endereco;
+    }
+}
